67f2ccb5eb7f6fa189d8431b781f8d0d0f65339d
jeremyw24 <jeremy@visionswest.com.au>
2024-08-14 16:29:39 +1000

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 // import vercelServerless from "@astrojs/vercel/serverless";
-import vercel from "@astrojs/vercel/serverless";
+//import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel(),
+  output: "static",
+  adapter: netlify()
 });
