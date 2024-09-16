c692a5cb8bf19c7db233570006f90ba97a458966
jeremyw24 <jeremy@visionswest.com.au>
2024-09-16 21:57:00 +1000

    fix: tweak footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
           <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray pt-[18px] pb-[33px]">
           Warm, Loving Home-Style Care.
           </p>
-          <div className="flex gap-5 md:gap-[54px]">
+          <div className="flex gap-2 md:gap-[54px]">
             <a href="https://www.facebook.com/treetopscattery">
               <Facebook />
             </a>
@@ -22,46 +22,46 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div className="col-span-1 flex flex-col gap-4">
-          <h5 className="text-primary-200 font-dm text-[22px] lg:text-[25px] leading-loose">
+        <div className="col-span-1 flex flex-col gap-2">
+          <h5 className="text-primary-200 font-dm text-[18px] lg:text-[25px] leading-loose">
             Pages
           </h5>
           <div className="flex flex-col gap-6">
             <a
               href="/about"
-              className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray"
+              className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray"
             >
               About Us
             </a>
             <a
               href="/services"
-              className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray"
+              className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray"
             >
               Services
             </a>
             <a
               href="/contact"
-              className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray"
+              className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray"
             >
               Contact Us
             </a>
           </div>
         </div>
-        <div className="col-span-1 flex flex-col gap-4">
-          <h5 className="text-primary-200 font-dm text-[22px] lg:text-[25px] leading-loose">
+        <div className="col-span-1 flex flex-col gap-2">
+          <h5 className="text-primary-200 font-dm text-[18px] lg:text-[18px] leading-loose">
             Services
           </h5>
           <div className="flex flex-col gap-6">
-            <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
+            <p className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray">
               Cat Boarding
             </p>
-            <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
+            <p className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray">
               Guinea Pig Boarding
             </p>
-            <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
+            <p className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray">
               Rabbit Boarding
             </p>
-            <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
+            <p className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray">
               Pet Taxi
             </p>
           </div>
@@ -71,13 +71,13 @@ const Footer = () => {
             Contact
           </h5>
           <div className="flex flex-col gap-6">
-            <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
+            <p className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray">
               222 Coolart Road, Moorooduc Victoria 3913
             </p>
-            <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
+            <p className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray">
               bookings@treetopscattery.com.au
             </p>
-            <p className="tracking-tight text-base lg:text-[22px] lg:leading-[33px] font-jost text-text-gray">
+            <p className="tracking-tight text-base lg:text-[18px] lg:leading-[33px] font-jost text-text-gray">
               (03)59 77 5328
             </p>
           </div>
