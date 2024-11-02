72ef27cfe65ae6d578776a5a9b1331dc06d35978
jeremyw24 <jeremy@visionswest.com.au>
2024-11-02 18:15:31 +1100

    chore: add tracking and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import partytown from '@astrojs/partytown';
 // import vercelServerless from "@astrojs/vercel/serverless";
 //import vercel from "@astrojs/vercel/serverless";
 
 import netlify from "@astrojs/netlify/functions";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://treetopscattery.com.au',
+  integrations: [tailwind(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()],
   output: "static",
   adapter: netlify()
 });
